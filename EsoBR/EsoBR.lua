8cf3c5eb86f3c8059ae035380d8008703ae32915
Sharlikran <danielhmpdx16@gmail.com>
2021-05-30 10:38:27 -0700

    Version: 1.3.10.0

--- EsoBR/EsoBR.lua
+++ EsoBR/EsoBR.lua
@@ -3,7 +3,7 @@ local LMP = LibMediaProvider
 
 local EsoBR = {}
 EsoBR.Flags = { "en", "br" }
-EsoBR.Version = "1.3.7.6"
+EsoBR.Version = "1.3.10.0"
 EsoBR.Name = "EsoBR"
 EsoBR.Defaults = {
 	Anchor = { BOTTOMRIGHT, BOTTOMRIGHT, 0, 7 },
